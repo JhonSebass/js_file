0057e7610147929cb718e76689a2603c2bb4aa15
= <=>
2022-03-07 13:26:23 -0500

    completo 03

---
+++ 03/script.js
@@ -0,0 +1,6 @@
+
+let meses_del_anio = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
+
+meses_del_anio.forEach(function(mes,indice,arreglo){
+	console.log(mes,indice);
+})
